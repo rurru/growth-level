0dba521e69e08d87f9f671cea7ec6ac9db21e2f2
rurru <44536425+rurru@users.noreply.github.com>
2019-06-25 20:35:46 -0700

    Separate tab imports included

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React, { useEffect, useContext, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import AppContext from './context/context.js';
+
+const TaskList = React.lazy(() => {
+  return import('./containers/TaskList/TaskList');
+});
+
+const QuestLog = React.lazy(() => {
+  return import('./containers/QuestLog/QuestLog');
+});
+
+const RewardList = React.lazy(() => {
+  return import('./containers/RewardList/RewardList');
+});
 
 const app = props => {
   return (<div></div>);
