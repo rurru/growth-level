ffdbb9fd7126ab8f3adcfe46f4aa8e6bb97dd05c
Robin Preibisius <rpreibisius@gmail.com>
2019-08-06 20:09:51 -0700

    UI css added

--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './ui.css';
+import logo from '../../assets/GL-logo.png';
 
 const NavBar = (props) => {
-  return (<div id = "navbar"></div>);
+  return (
+  <div id = "navbar">
+    <div id = "branding">
+      <img src = {logo} />
+      Growth Level
+    </div>
+  </div>);
 }
 
 export default NavBar;
