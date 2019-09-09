a182a79bbd7a41439ef2e512e89dcabf3bea3996
Robin Preibisius <rpreibisius@gmail.com>
2019-09-08 20:34:35 -0700

    Minor tweak to authentication imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { FirebaseAuth } from 'react-firebaseui'
+import authConfig from './config';
+import { StyledFirebaseAuth } from 'react-firebaseui/StyledFirebaseAuth'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
