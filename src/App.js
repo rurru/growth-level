ef929d1600ae759f7b4f7c3684d9c4a47daf6f8f
Robin Preibisius <44536425+rurru@users.noreply.github.com>
2019-10-19 11:41:00 -0700

    Refactor state/remove context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useContext, Suspense } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import AppContext from './context/context';
 import XPBar from './components/XPBar';
 import NavBar from './components/UI/NavBar';
 import TaskList from './containers/TaskList';
@@ -15,29 +14,50 @@ const RewardList = React.lazy(() => {
 });
 
 const App = (props) => {
-  useEffect(
-    () => {"tobecompletedlater";}, []
+  const [xp, setXp] = useState(37);
+  const [progress, setProgress] = useState({current: 0, toLevel: 100});
+  const [level, setLevel] = useState(1);
+  const [multiplier, setMultiplier] = useState(2); //1 = fast, 2 = balanced, 3 = slow
+  const [levelXp, setLevelXp] = useState(200);
+
+
+  useEffect(() => {
+    updateXP(0);}, []
   );
 
-  const calcXpToLevel = (level, multiplier) => {
+  const calcXpToLevel = () => {
     Math.round(multiplier * 100 * (Math.pow(level, 3)));
   }
 
+  const updateXP = (points) => {
+    let newXP = xp + points;
+    if (newXP > levelXP) {
+      setLevel(level => level + 1);
+      setLevelXp(calcXpToLevel());
+      newXP -= levelXp;
+    }
+
+    setXp(newXP);
+    let xpPercent = (xp * 100) / levelXp;
+    setProgress({current: xpPercent, toLevel: 100 - xpPercent});
+  }
+
   return (
-  <AppContext.Provider>
     <div id = "app">
       <NavBar />
-      <XPBar />
+      <XPBar progress = {progress} />
       <Suspense fallback = {<p>Loading Tasks...</p>} >
         <Switch>
-          <Route path="questlog" render={props => <QuestLog />} />
-          <Route path="rewardList" render={props => <RewardList />} />
-          <Route path="taskList" render={props => <TaskList />} />
+          <Route path="questlog" render={props => 
+            <QuestLog {...props} update = {(p) => updateXP(p)} />} level = {level} />
+          <Route path="rewardList" render={props => 
+            <RewardList {...props} />} />
+          <Route path="taskList" render={props => 
+            <TaskList {...props} update = {(p) => updateXP(p)} level = {level} />} />
           <Redirect to="taskList" />
         </Switch>
       </Suspense>
-    </div>
-  </AppContext.Provider>);
+    </div>);
 }
 
 export default App;
