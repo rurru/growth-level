0f52076013cd92d8b7a47f2ef90a7f56117fdc3d
Robin Preibisius <rpreibisius@gmail.com>
2019-11-11 18:05:23 -0800

    Menu styling

--- src/components/UI/MainMenu.js
+++ src/components/UI/MainMenu.js
@@ -4,6 +4,8 @@ import './ui.css';
 
 const MainMenu = (props) => {
     const [buttonStyle, setButtonStyle] = useState(null);
+    const menuStyle = { width: "auto", padding: "0", boxShadow: "-8px 8px 5px #ccc"};
+    const subMenuStyle = {...menuStyle, textAlign: "center"};
 
     const toggleMenu = () => {
       let settingsActive = buttonStyle != null;
@@ -12,13 +14,23 @@ const MainMenu = (props) => {
 
   return (
       <Popup trigger = { <div id = "menu-button" style = {buttonStyle} >
-                          <i className ="fas fa-bars"></i>
-                         </div>} onOpen = {toggleMenu} onClose = {toggleMenu} 
-                         position ='bottom right'>
+                          <i className="fas fa-bars"></i> </div>} 
+                          onOpen={toggleMenu} onClose={toggleMenu} arrow={false}
+                         contentStyle={menuStyle} position='bottom right'>
         <div className="menu">
           <div className="menu-item">Set Path</div>
-          <div className="menu-item"> Set Categories </div>
-          <div className="menu-item"> Set Leveling Speed</div>
+          <div className="submenu-item"> Edit Categories </div>
+
+          <Popup trigger={<div className="menu-item"> Set Leveling Speed </div>}
+                            position="left top" on="hover"  arrow = {false}
+                            contentStyle={subMenuStyle}>
+            <div className="subMenu">
+              <div className="submenu-item">Slow</div>
+              <div className="submenu-item">Balanced</div>
+              <div className="submenu-item">Fast</div>
+            </div>
+          </Popup>
+
         </div>
       </Popup>
     );
