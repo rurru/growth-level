e9ee9e0fffc09eaf94486019898656a659116d5b
Robin Preibisius <44536425+rurru@users.noreply.github.com>
2019-11-28 12:26:53 -0800

    Implemented color picker

--- src/components/Editors/CategoryEditor.js
+++ src/components/Editors/CategoryEditor.js
@@ -1,13 +1,38 @@
 import React, { useState }  from 'react';
 import "./editors.css";
+import { categoryColors } from "./../../constants"
 
 const CategoryEditor = (props) => {
     const [colorPickerStyle, setColorPickerStyle] = useState({display: "none"});
+    const [currentCategory, setCurrentCategory] = useState(0);
+
+    const selectCategory = (cat) => {
+        setColorPickerStyle({display: "block", top: -227 + (cat+2)*36 + "px"});
+        setCurrentCategory(cat); 
+    }
+
+    const selectColor = (color) => {
+        let newCategories = props.categories;
+        newCategories[currentCategory].color = color;
+        setColorPickerStyle({display: "none"});
+        props.update("categories", newCategories);
+    }
+
+    const updateName = (name) => {
+        
+    }
 
     return (
     <div className = "settings-table" >    
         <div className = "category-color-picker" style = {colorPickerStyle} >
-            Choose a Color
+            <div className = "options-header">Choose a Color</div>
+            { categoryColors.map(color =>
+                <div className = "category-color-option" 
+                     key = {color.color}
+                     style = {{backgroundColor: color.color}} 
+                     onClick = {() => selectColor(color)} />
+                )
+            }
         </div>
         <div className = "table-row table-header">
             <div className = "table-column">Name</div>
@@ -19,8 +44,7 @@ const CategoryEditor = (props) => {
                 <div className = "table-column">
                     <div className = "category-color" 
                         style = {{backgroundColor: cat.color.color}}
-                        onClick = {() => setColorPickerStyle({display: "block", top: -261 + (i+2)*36 + "px"})} > 
-                    </div>
+                        onClick = {() => selectCategory(i)} /> 
                 </div>
             </div>
         )}
