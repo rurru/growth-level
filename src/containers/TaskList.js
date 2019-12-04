60dc18eac5cc2797628e9079cf7a700565d1d29c
Robin Preibisius <44536425+rurru@users.noreply.github.com>
2019-12-03 22:18:01 -0800

    Handler functions for tasklist buttons

--- src/containers/TaskList.js
+++ src/containers/TaskList.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useContext} from 'react';
+import Popup from "reactjs-popup";
 import TaskEditor from '.././components/Editors/TaskEditor';
 import './lists.css';
 //import TaskItem from 'TaskItem';
@@ -8,9 +9,18 @@ const TaskList = (props) => {
         Initializate();}, []
       );
  */
-    const [editMode, seteEditMode] = useState(false);
+    const [editTask, setEditTask] = useState(false);
+    const [editMode, setEditMode] = useState(false);
     const [tasks, setTasks] = useState([]);
 
+    const toggleTaskEditor = (editorOpen) => {
+
+    }
+
+    const toggleEditMode = () => {
+        const newEditMode = !editMode;
+        setEditMode(newEditMode);
+    }
 
     const handleTaskClick = (level) => {
     }
@@ -19,8 +29,18 @@ const TaskList = (props) => {
  
     return (
         <div id = "tasklist" className = "container">
-            <div className = "add-button"><i class="fas fa-plus"></i></div>
-            <div className = "edit-button"><i class="fas fa-pen"></i></div>
+            <div className = "add-button" >
+            <Popup open = {editTask} onClose = {() => toggleTaskEditor(false)}
+                  contentStyle = {{width: "auto"}}>
+            </Popup>
+                <i class="fas fa-plus"></i>
+            </div>
+            <div className = "edit-button" onClick={() => toggleEditMode()}>
+                <i class="fas fa-pen"></i>
+            </div>
+
+            
+            Edit mode is {editMode ? "on" : "off"}
             {//tasks.map(task =>
                // <TaskItem onTaskClick = {(level) => handleTaskClick(level)}
                     
