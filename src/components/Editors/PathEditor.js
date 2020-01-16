c679adbd8e9d6356cce35a91befeff1119e256b0
Robin Preibisius <44536425+rurru@users.noreply.github.com>
2020-01-16 13:49:48 -0800

    Change path edit icon

--- src/components/Editors/PathEditor.js
+++ src/components/Editors/PathEditor.js
@@ -46,9 +46,10 @@ const PathEditor = (props) => {
                 <div className = "options-header">Select Path Categories</div>
             </div>
             <div className = "table-row table-header">
-                <div className = "table-column">Name</div>
-                <div className = "table-column">Color</div>
+                <div className = "table-column">Path Name</div>
+                <div className = "table-column"></div>
             </div>
+        
         {paths.slice(1).map ((path, i) => 
             <div className = "table-row" key = {path.id}>
                 <div className = "table-column">
@@ -57,17 +58,20 @@ const PathEditor = (props) => {
                         onChange={(e) => updateName(e, i)} />  
                 </div>
                 <div className = "table-column">
-                    <div className = "path-edit-button" 
-                        onClick = {() => selectPath(i)} /> 
+                    <div className="edit" onClick = {() => selectPath(i)} > 
+                          <i className="fas fa-pencil-alt"></i>
+                    </div>
                 </div>
                 <div className = "delete" onClick = {() => deletePath(i)}>x</div>
             </div>
         )}
+        
         <div className = "table-row" >     
             <input type="text" className = "settings-input"
                 placeholder="Enter new path name" 
                 onKeyDown={keyPressed} /> 
         </div>
+
         <div className = "table-row button submit-button" onClick = {() => props.update("paths", paths)}>
             Submit Changes
         </div>
