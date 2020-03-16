4c2734a71904a7cbeaf60c06ca816045b1eea2a3
Robin Preibisius <rpreibisius@gmail.com>
2020-03-15 21:37:29 -0700

    Add cloudinary support

--- src/components/Editors/RewardEditor.js
+++ src/components/Editors/RewardEditor.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+import {uploadConfig} from '../Config';
 
 const RewardEditor = (props) => {
-  return (<div></div>);
+  return (
+  <div>
+    <Dropzone
+      multiple={false}
+      accept="image/*"
+      onDrop={this.onImageDrop.bind(this)}>
+      <p>Drop an image or click to select a file to upload.</p>
+    </Dropzone>
+  </div>
+  );
 }
 
 export default RewardEditor;
