62ce7470d87944e4973e83fcef385b986069ba9b
Robin Preibisius <rpreibisius@gmail.com>
2020-03-20 20:14:43 -0700

    Fix import errors

--- src/components/Editors/RewardEditor.js
+++ src/components/Editors/RewardEditor.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
-import {uploadConfig} from '../Config';
+import config from '../../Config';
 
 const RewardEditor = (props) => {
+
+  const [uploadedFile, setUploadedFile] = useState('');
+
+  const onImageDrop = (files) => {
+    setUploadedFile(files[0]);
+    handleImageUpload(files[0]);
+  }
+
+  const handleImageUpload = (file) => {
+    let upload = request.post(config.uploadURL)
+                        .field('upload_preset', config.uploadPreset )
+                        .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
+
   return (
-  <div>
+  <div id = "reward-editor">
     <Dropzone
       multiple={false}
       accept="image/*"
-      onDrop={this.onImageDrop.bind(this)}>
+      onDrop={onImageDrop}>
       <p>Drop an image or click to select a file to upload.</p>
     </Dropzone>
   </div>
