9c558a45e646bdf50a873de6280ab561c4a40adc
Robin Preibisius <rpreibisius@gmail.com>
2020-03-29 16:58:56 -0700

    Reward item component

--- src/components/ListItems/RewardItem.js
+++ src/components/ListItems/RewardItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const RewardItem = (props) => {
-  return (<div></div>);
+  const classes = props.editing ? "reward-item editing-reward" : "reward-item";
+
+  return (
+  <div className="reward-item" key = {props.id} onClick = {() => props.onRewardClick(props.id)}>       
+      <img src = {props.url} className = "reward-image" />
+      <span className="level">{props.level}</span>
+      <div className = "reward-item-label">{props.name}</div>
+  </div>);
 }
 
 export default RewardItem;
